8ad8c94cc8957eb6e2bbc4760a8448116f6810f7
Ayush Pandey <ayush@Ayushs-MacBook-Pro.local>
2023-12-26 10:36:24 +1100

    hard challenges calculator and todo-list solved

--- Week1/assignments/01-js/hard/calculator.js
+++ Week1/assignments/01-js/hard/calculator.js
@@ -16,6 +16,46 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+
+constructor (result){
+  this.result = 0;
+}
+
+add(num){
+  this.result += num;
+}
+
+subtract(num){
+  this.result -= num;
+}
+
+multiply(num){
+  this.result *= num;
+}
+
+divide(num){
+  if (num === 0){
+    throw new Error();
+  }
+  this.result /= num;
+}
+
+clear(){
+  this.result = 0;
+}
+
+getResult(){
+  return this.result;
+}
+
+calculate(str){
+  if (eval(str) == Infinity) {
+    throw new Error();
+  }
+  this.result = eval(str);
+}
+
+}
 
 module.exports = Calculator;
