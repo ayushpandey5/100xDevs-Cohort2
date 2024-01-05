b3f9c095caecd9eacf2f5ba2209328401018038c
Ayush Pandey <ayushpandey5@icloud.com>
2024-01-05 13:07:18 +1100

    simple zod validate practice

--- Week3/05-zod/index.js
+++ Week3/05-zod/index.js
@@ -1,2 +1,19 @@
 const express = require("express");
-const z = require("zod");
+const zod = require("zod");
+
+const app = express();
+
+app.use(express.json());
+
+const user = zod.object({
+  email: zod.string().email(),
+  password: zod.string().min(8),
+});
+
+app.post("/", function (req, res) {
+  const users = req.body.user; //just req.body if we are sending a single object
+  const response = user.safeParse(users);
+  res.send(response);
+});
+
+app.listen(3000);
