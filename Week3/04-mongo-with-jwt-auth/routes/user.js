3e2235bfd3ad4c0440792dcb65058a5758a5ef35
Ayush Pandey <ayushpandey5@icloud.com>
2024-01-12 12:51:01 +1100

    user signup, signin and jwt token save

--- Week3/04-mongo-with-jwt-auth/routes/user.js
+++ Week3/04-mongo-with-jwt-auth/routes/user.js
@@ -2,15 +2,52 @@ const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
 const jwt = require("jsonwebtoken");
+const { User } = require("../db");
 const JWT_SECRET = "JsonWebTokenPassword@123";
 
 // User Routes
-router.post("/signup", (req, res) => {
-  // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const existingUser = await User.findOne({
+      username: username,
+      password: password,
+    });
+    if (existingUser) {
+      return res.status(403).json({ error: "User already exists" });
+    }
+    const newUser = new User({
+      username: username,
+      password: password,
+    });
+
+    await newUser.save();
+    return res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-router.post("/signin", (req, res) => {
-  // Implement admin signup logic
+router.post("/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const user = await User.findOne({
+      username: username,
+      password: password,
+    });
+    if (user) {
+      const token = jwt.sign(username, JWT_SECRET);
+      user.token = token;
+      await user.save();
+      return res.status(403).json({ token: token });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 router.get("/courses", (req, res) => {
