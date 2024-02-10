96da87c0a74e523d76fca706bb2e995c501606b1
Ayush Pandey <ayushpandey5@icloud.com>
2024-02-10 17:52:29 +1100

    todo routes post,get,put complete....need to update it later for getting user-based post/get/put

--- Week5/todo-fullstack/todo-backend/routes/todoRoutes.js
+++ Week5/todo-fullstack/todo-backend/routes/todoRoutes.js
@@ -11,23 +11,26 @@ router.post("/todo", async(req,res) => {
     if(!parsedPayload.success){
         return res.status(411).json({error: "Bad request inputs"})
     }
-    const todo = new Todo(parsedPayload);
+    const todo = new Todo(parsedPayload.data);
     await todo.save();
-    return res.status(200).json(success: `${parsedPayload}`)
+    return res.status(200).json({success : `${parsedPayload}`})
 
 })
 
 router.get("/todos", async(req, res) => {
-
+    const todos = await Todo.find({})
+    return res.status(200).json({ data: todos})
 })
 
-router.put("/completed", (req, res) =>{
+router.put("/completed", async (req, res) =>{
     const payload = req.body
-    const parsedPayload = updateTodo(payload)
+    const parsedPayload = updateTodo.safeParse(payload)
     if(!parsedPayload.success){
         return res.status(401).json({error: "Bad request input"})
     }
-    const todo = await Todo.findOne({ _id: parsedPayload})
+    const todo = await Todo.updateOne({ _id: parsedPayload.data.id}, {
+        isComplete: true
+    })
 })
 
 module.exports = router;
