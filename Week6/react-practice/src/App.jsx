0fef9e3ce332364df3a4e40eaff43b7ffede6328
Ayush Pandey <ayushpandey5@icloud.com>
2024-02-24 19:28:08 +1100

    useEffect with dependency

--- Week6/react-practice/src/App.jsx
+++ Week6/react-practice/src/App.jsx
@@ -1,46 +1,49 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header.jsx'
-import HeaderBtn from './components/HeaderBtn.jsx'
-import Todo from './components/Todo.jsx'
-import CardWrapper from './components/CardWrapper.jsx'
-import TextComponent from './components/TextComponent.jsx'
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import HeaderBtn from "./components/HeaderBtn.jsx";
+import Todo from "./components/Todo.jsx";
+import CardWrapper from "./components/CardWrapper.jsx";
+import TextComponent from "./components/TextComponent.jsx";
 
 function App() {
-// const [todos,setTodos] = useState([{
-//   title: "Eat food",
-//   description: "Eat food"
-// },
-// {
-//   title: "Go to gym",
-//   descrition: "Go to gym"
-// }])
-
-// function addTodo(){
-//   setTodos([...todos,{
-//     title: "Nice",
-//     description: "new todo"
-//   }])
-// }
+  const [todos, setTodos] = useState([]); // Initialize todos to an empty array
+  useEffect(() => {
+    fetch("https://sum-server.100xdevs.com/todos").then(async (res) => {
+      const json = await res.json();
+      setTodos(json.todos);
+    });
+    //dependency, runs the useEffect if it changes
+  }, [todos]);
+  // function addTodo(){
+  //   setTodos([...todos,{
+  //     title: "Nice",
+  //     description: "new todo"
+  //   }])
+  // }
 
   return (
     <>
-      {/* <button onClick={addTodo}>Add Todo</button>
-      {todos.map((todo) => <Todo title={todo.title} key={todo.title} description={todo.description}></Todo>
-      )} */}
+      {/* <button onClick={addTodo}>Add Todo</button> */}
+      {todos.map((todo) => (
+        <Todo
+          title={todo.title}
+          key={todo.title}
+          description={todo.description}
+        ></Todo>
+      ))}
 
-      <CardWrapper> 
+      {/* <CardWrapper>
         <TextComponent />
         <TextComponent />
       </CardWrapper>
 
-      <CardWrapper> 
+      <CardWrapper>
         <TextComponent />
         <TextComponent />
-      </CardWrapper>
+      </CardWrapper> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
