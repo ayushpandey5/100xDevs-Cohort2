54c658b01ddaf59bed0e7c5971ff5709cfea40bf
Ayush Pandey <ayushpandey5@icloud.com>
2024-03-22 17:06:04 +1100

    revenue card

--- Week8/tailwind-practice/src/App.jsx
+++ Week8/tailwind-practice/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import RevenueCard from './components/RevenueCard'
 
 function App() {
 
   return (
     <>
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+      <RevenueCard title={"Amount Pending"} amount={"92,000"} orderCount={18}/>
+      <RevenueCard title={"Amount Pending"} amount={"92,000"} orderCount={18}/>
+      <RevenueCard title={"Amount Pending"} amount={"92,000"} orderCount={18}/>
+      </div>
+    
       {/* <div style={{display: "flex"}}>
       <div style={{backgroundColor: "red", width: "100px", height: "100px"}}></div>
       <div style={{backgroundColor: "yellow",  width: "100px", height: "100px"}}></div>
@@ -11,26 +18,25 @@ function App() {
       </div> */}
 
       {/* Flex */}
-      <div className='flex justify-between'>
+      {/* <div className='flex justify-between'>
         <div>hi</div>
         <div>hi</div>
         <div>hi</div>
-      </div>
-
+      </div> */}
 
       {/* Grids */}
-      <div className='grid grid-cols-10'>
+      {/* <div className='grid grid-cols-10'>
         <div className='col-span-4 bg-black'>hi</div>
         <div className='col-span-4 bg-red-300'>hi</div>
         <div className='col-span-2 bg-yellow-200'>hi</div>
-      </div>
+      </div> */}
 
       {/* Responsive */}
-      <div className='bg-red-100 md:bg-blue-200 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+      {/* <div className='bg-red-100 md:bg-blue-200 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
         <div className=''>Div1</div>
         <div className=''>Div2</div>
         <div className=''>Div3</div>
-      </div>
+      </div> */}
     </>
   )
 }
